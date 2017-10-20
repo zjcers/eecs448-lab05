13c933d40cb8dccd63ad2ba635eb471f0d99d32c
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-19 22:03:30 -0500

    Made the SQL tables use InnoDB instead of the default so that foreign keys are respected.

--- exercise1.sql
+++ exercise1.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Users (user_id VARCHAR(255),
                     PRIMARY KEY(user_id)
-);
+) ENGINE=InnoDB;
 CREATE TABLE Posts (post_id INT NOT NULL AUTO_INCREMENT,
                     content TEXT,
                     author_id VARCHAR(255),
                     PRIMARY KEY(post_id),
-                    FOREIGN KEY(author_id) REFERENCES Users(user_id)
-);
+                    CONSTRAINT FOREIGN KEY(author_id) REFERENCES Users(user_id)
+) ENGINE=InnoDB;
