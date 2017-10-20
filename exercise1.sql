930765b11755e4bc9dc3d72c839bd59871053b57
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-19 21:23:09 -0500

    Fixed the database schema (forgot to read...)

--- exercise1.sql
+++ exercise1.sql
@@ -1,9 +1,9 @@
-CREATE TABLE Users (user_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Users (user_id VARCHAR(255),
                     PRIMARY KEY(user_id)
 );
 CREATE TABLE Posts (post_id INT NOT NULL AUTO_INCREMENT,
                     content TEXT,
-                    author_id INT NOT NULL,
+                    author_id VARCHAR(255),
                     PRIMARY KEY(post_id),
                     FOREIGN KEY(author_id) REFERENCES Users(user_id)
 );
